1b30ec325a76262e78ca4059411448baa0dd83fe
John <john@real-currents.com>
2020-12-06 10:30:27 -0500

    debug auth errors

--- api/quest/index.js
+++ api/quest/index.js
@@ -10,7 +10,7 @@ const serverDir = `${__dirname}/quest/server`.replace("quest/quest", "quest");
 function basicAuth (req) {
     console.log(JSON.stringify(req.body || req.headers));
 
-    ls = execSync(`ls -lA ${serverDir}/output`);
+    ls = execSync(`ls -lA ${serverDir}`);
 
     let errorStep = "";
 
@@ -22,43 +22,34 @@ function basicAuth (req) {
             .replace(/\+/g, ' ')
             .replace(/%3D/g, '=')
             .match(/authorization=(Basic .+$)/).join("\n ");
-        
-        req.body = (req.headers.authorization || req.body.Authorization || req.body);
 
-        // check for basic auth header
-        // if ((!!req.headers.authorization && req.headers.authorization.indexOf('Basic') > -1) ||
-        //     (!!req.body.Authorization && req.body.Authorization.indexOf('Basic') > -1) ||
-        //     (!!req.body.match(/authorization/) && req.body.indexOf('Basic') > -1)
-        // ) {
+        const authorization = (req.body.Authorization || req.body || req.headers.authorization)
+            .replace(/\+/g, ' ')
+            .replace(/%3D/g, '=')
+            .match(/authorization=(Basic .+$)/)[1];
 
-            const authorization = (req.headers.authorization || req.body.Authorization || req.body)
-                .replace(/\+/g, ' ')
-                .replace(/%3D/g, '=')
-                .match(/authorization=(Basic .+$)/)[1];
+        req.body = authorization;
 
-            req.body = authorization;
-
-            // verify auth credentials
-            const base64Credentials = authorization.split(' ')[1];
-            const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
-            const [username, password] = credentials.split(':');
-            // const user = await userService.authenticate({ username, password });
-            // if (!user) {
-            //     return res.status(401).json({ message: 'Invalid Authentication Credentials' });
-            // }
-
-            if (password === "enter" && (
-                    username === "john" ||
-                    username === "hannah" ||
-                    username === "devon" ||
-                    username === "autum" ||
-                    username === "jasper" ||
-                    username === "dave"
-                )
-            ) {
-                return true;
-            }
+        // verify auth credentials
+        const base64Credentials = authorization.split(' ')[1];
+        const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+        const [username, password] = credentials.split(':');
+        // const user = await userService.authenticate({ username, password });
+        // if (!user) {
+        //     return res.status(401).json({ message: 'Invalid Authentication Credentials' });
         // }
+
+        if (password === "enter" && (
+                username === "john" ||
+                username === "hannah" ||
+                username === "devon" ||
+                username === "autum" ||
+                username === "jasper" ||
+                username === "dave"
+            )
+        ) {
+            return true;
+        }
 
     } catch (err) {
         error = Object.create({}, (!!err) ? err : {});
@@ -81,21 +72,21 @@ module.exports = async function (context, req) {
         body: responseMessage
     };
 
-    ls = execSync(`ls -lA ${serverDir}/output`);
-
     if (basicAuth(req)) try {
         // const stdout = execSync(`node node_modules/tiddlywiki/tiddlywiki.js ${serverDir} --build index`);
         // console.log(`stdout: ${stdout}`);
 
         // // Pass the command line arguments to the boot kernel
-        // $tw.boot.argv = [
-        //     serverDir,
-        //     '--build',
-        //     'index'
-        // ];
-        //
-        // // Boot the TW5 app
-        // await $tw.boot.boot();
+        $tw.boot.argv = [
+            serverDir,
+            '--build',
+            'index'
+        ];
+
+        // Boot the TW5 app
+        await $tw.boot.boot();
+
+        ls = execSync(`ls -lA ${serverDir}/output`);
 
         context.res = {
             // status: 200, /* Defaults to 200 */
