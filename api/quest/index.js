03f389201ef389716a467b5bcc13ab4939f41bb9
John <john@real-currents.com>
2020-12-06 12:05:39 -0500

    working build

--- api/quest/index.js
+++ api/quest/index.js
@@ -1,6 +1,6 @@
 const { execSync } = require("child_process");
 const fs = require("fs");
-const $tw = require("tiddlywiki/boot/boot.js").TiddlyWiki();
+// const $tw = require("tiddlywiki/boot/boot.js").TiddlyWiki();
 
 let error = null;
 let ls = '';
@@ -15,14 +15,6 @@ function basicAuth (req) {
     let errorStep = "";
 
     try {
-        errorStep = "(!!req.headers.authorization && req.headers.authorization.indexOf('Basic') > -1): " + (!!req.headers.authorization && req.headers.authorization.indexOf('Basic') > -1);
-        errorStep = "(!!req.body.Authorization && req.body.Authorization.indexOf('Basic') > -1): " + (!!req.body.Authorization && req.body.Authorization.indexOf('Basic') > -1);
-        errorStep = "(!!req.body.match(/authorization/) && req.body.indexOf('Basic') > -1): " + (!!req.body.match(/authorization/) && req.body.indexOf('Basic') > -1);
-        errorStep = (req.body)
-            .replace(/\+/g, ' ')
-            .replace(/%3D/g, '=')
-            .match(/authorization=(Basic .+$)/).join("\n ");
-
         const authorization = (req.body.Authorization || req.body || req.headers.authorization)
             .replace(/\+/g, ' ')
             .replace(/%3D/g, '=')
@@ -33,44 +25,33 @@ function basicAuth (req) {
         // verify auth credentials
         const base64Credentials = authorization.split(' ')[1];
         const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
-        const [username, password] = credentials.split(':');
+        const [ username, password ] = credentials.split(':');
         // const user = await userService.authenticate({ username, password });
         // if (!user) {
         //     return res.status(401).json({ message: 'Invalid Authentication Credentials' });
         // }
 
         if (password === "enter" && (
-                username === "john" ||
-                username === "hannah" ||
-                username === "devon" ||
-                username === "autum" ||
-                username === "jasper" ||
-                username === "dave"
-            )
-        ) {
+            username === "john" ||
+            username === "hannah" ||
+            username === "devon" ||
+            username === "autum" ||
+            username === "jasper" ||
+            username === "dave"
+        )) {
             return true;
         }
 
     } catch (err) {
         error = Object.create({}, (!!err) ? err : {});
-        error.message = "Error during check for authorization\n " + errorStep + "\n " +(error.message || '');
+        error.message = "Error during check for authorization\n " + (error.message || '');
     }
 
     return false;
 }
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    context.log('JavaScript HTTP trigger function received a request.');
 
     if (basicAuth(req)) try {
         const stdout = execSync(`node node_modules/tiddlywiki/tiddlywiki.js ${serverDir} --build index`);
@@ -93,6 +74,7 @@ module.exports = async function (context, req) {
                 // status: 200, /* Defaults to 200 */
                 body: fs.readFileSync(`${serverDir}/output/index.html`)
             };
+
         } else {
             context.res = {
                 // status: 200, /* Defaults to 200 */
@@ -100,8 +82,7 @@ module.exports = async function (context, req) {
                     "error": new Error(`Could not find ${serverDir}/output/index.html`),
                     "body": req.body,
                     "dir": serverDir,
-                    "ls": `${ls}`,
-                    "tw_boot": $tw.boot
+                    "ls": `${ls}`
                 })
             };
         }
@@ -117,8 +98,7 @@ module.exports = async function (context, req) {
                     "error": err,
                     "body": req.body,
                     "dir": serverDir,
-                    "ls": `${ls}`,
-                    "tw_boot": $tw.boot
+                    "ls": `${ls}`
                 })
             };
         }
